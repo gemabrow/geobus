8744f53b503338e54a3069f04516b182d8e553f5
gemabrow <gemabrow@ucsc.edu>
2015-10-13 20:44:30 -0700

    in process of adapting XML parsing tutorial

---
+++ app/src/main/java/com/sprint1/geobus_map/NetworkActivity.java
@@ -0,0 +1,122 @@
+package com.sprint1.geobus_map;
+import android.os.AsyncTask;
+import org.xmlpull.v1.XmlPullParserException;
+import java.io.IOException;
+
+/**According to user's preferences, performs background async activity
+ * of downloading and parsing xml file for updates to main map activity
+ * Created by gerald on 10/13/15.
+ */
+public class NetworkActivity extends android.app.Activity {
+    public static final String WIFI = "Wi-Fi";
+    public static final String ANY = "Any";
+    private static final String URL = "http://skynet.cse.ucsc.edu/bts/coord2.xml";
+
+    // Whether there is a Wi-Fi connection.
+    private static boolean wifiConnected = false;
+    // Whether there is a mobile connection.
+    private static boolean mobileConnected = false;
+    // Whether the display should be refreshed.
+    public static boolean refreshDisplay = true;
+    public static String sPref = null;
+
+    // Uses AsyncTask to download the XML feed from skynet.cse.ucsc.edu.
+    public void loadPage() {
+
+        if((sPref.equals(ANY)) && (wifiConnected || mobileConnected)) {
+            new DownloadXmlTask().execute(URL);
+        }
+        else if ((sPref.equals(WIFI)) && (wifiConnected)) {
+            new DownloadXmlTask().execute(URL);
+        } else {
+            // show error
+        }
+    }
+
+    // Implementation of AsyncTask to download
+    // XML feed from http://skynet.cse.ucsc.edu/bts/coord2.xml
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch ( IOException e ){
+                return getResources().getString(R.string.connection_error);
+            } catch ( XmlPullParserException e ) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        //need to adjust for drawing to Google Maps activity
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.main);
+        }
+    }
+
+    // Uploads XML from stackoverflow.com, parses it, and combines it with
+    // HTML markup. Returns HTML string.
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        StackOverflowXmlParser stackOverflowXmlParser = new StackOverflowXmlParser();
+        List<Entry> entries = null;
+        String title = null;
+        String url = null;
+        String summary = null;
+        Calendar rightNow = Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+        // Checks whether the user set the preference to include summary text
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean pref = sharedPrefs.getBoolean("summaryPref", false);
+
+        StringBuilder htmlString = new StringBuilder();
+        htmlString.append("<h3>" + getResources().getString(R.string.page_title) + "</h3>");
+        htmlString.append("<em>" + getResources().getString(R.string.updated) + " " +
+                formatter.format(rightNow.getTime()) + "</em>");
+
+        try {
+            stream = downloadUrl(urlString);
+            entries = stackOverflowXmlParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+        // Each Entry object represents a single post in the XML feed.
+        // This section processes the entries list to combine each entry with HTML markup.
+        // Each entry is displayed in the UI as a link that optionally includes
+        // a text summary.
+        for (Entry entry : entries) {
+            htmlString.append("<p><a href='");
+            htmlString.append(entry.link);
+            htmlString.append("'>" + entry.title + "</a></p>");
+            // If the user set the preference to include summary text,
+            // adds it to the display.
+            if (pref) {
+                htmlString.append(entry.summary);
+            }
+        }
+        return htmlString.toString();
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+}
