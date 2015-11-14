81e870334c7cb9b95ed463c34a07c340645cab46
jolguzma <jolguzma@ucsc.edu>
2015-11-14 13:12:27 -0800

    Added a fragment class to deal with bus schedules also added the xml for that fragment class, added strings that i used for testing the fragment class

--- app/src/main/java/com/bussquad/geobus/MapsActivity.java
+++ app/src/main/java/com/bussquad/geobus/MapsActivity.java
@@ -1,69 +1,98 @@
 package com.bussquad.geobus;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.androidmapsextensions.GoogleMap;
-import com.androidmapsextensions.GoogleMap.OnMarkerClickListener;
-import com.androidmapsextensions.Marker;
-import com.androidmapsextensions.MarkerOptions;
-import com.androidmapsextensions.OnMapReadyCallback;
-import com.androidmapsextensions.SupportMapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMarkerClickListener {
-    private final static int MARKER_UPDATE_INTERVAL = 2000; // in milliseconds
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , GoogleMap.OnMarkerClickListener {
     private static final String TAG = "MapsActivity";
+
+    private BusScheduleFragment busScheduleFragment;
     public static MapsActivity activity;
-    private final Handler locationHandler = new Handler();
-    private final JsonFileReader test = new JsonFileReader();
-    private Boolean showStops = false;
+
+    private Boolean showStops = true;
+    private Boolean infoWindowActive = false;
     private GoogleMap mMap;
     private ArrayList<BusStop> busStops;
+    private JsonFileReader test =  new JsonFileReader();
+
+    Handler locationHandler = new Handler();
+    final static int MARKER_UPDATE_INTERVAL = 2000; // in milliseconds
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+
     // Map is used to ensure bus markers are not duplicated, as
     // Google Maps V2 for Android has no method to uniquely ID
     // a marker according to input
     private Map<String, Marker> busMarkers = new HashMap<String, Marker>();
-    private final Runnable updateMarkers = new Runnable() {
-        @Override
-        public void run() {
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast;
-            //if data connection exists, fetch bus locations
-            if (NetworkUtil.isConnected(context)) {
-                toast = Toast.makeText(context, "connecting...", duration);
-                NetworkActivity networkActivity = new NetworkActivity();
-                networkActivity.load();
-            } else {
-                if (NetworkUtil.isConnecting(context)) {
-                    toast = Toast.makeText(context, "waiting for connection", duration);
-                } else {
-                    duration = Toast.LENGTH_LONG;
-                    toast = Toast.makeText(context, "no network connection", duration);
-                }
-            }
-            if (busMarkers.isEmpty())
-                toast.show();
-
-            locationHandler.postDelayed(this, MARKER_UPDATE_INTERVAL);
-        }
-    };
     private Map<String, Marker> visibleMarkers = new HashMap<String, Marker>();
+    /* Given a list of xml_markers (defined in TransitInfoXmlParser),
+     * update the map display such that new buses are added as type Marker to mMap,
+     * and previously displayed bus markers are moved to their new coordinates
+     */
+
+    public void stopBackgroundData() {
+        locationHandler.removeCallbacks(updateMarkers);
+    }
+
+    public void startBackgroundData() {
+        locationHandler.postDelayed(updateMarkers, MARKER_UPDATE_INTERVAL);
+    }
+
+    public void setMarkers(List<Bus> buses){
+        Map<String, Marker> updatedBusMarkers = new HashMap<String, Marker>();
+
+        for(Bus bus: buses){
+            Log.i(TAG, bus.toString());
+            // if marker exists, move its location
+            Marker bus_marker = busMarkers.get(Integer.toString(bus.bus_id));
+            // if not, add new marker
+            if(bus_marker == null){
+                bus_marker = mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(bus.lat, bus.lng))
+                        .title(bus.route)
+                        .snippet("Bus ID: " + Integer.toString(bus.bus_id))
+                        .draggable(true)
+                        .icon(BitmapDescriptorFactory.defaultMarker(bus.color)));
+            }
+            else {
+                bus_marker.setPosition(new LatLng(bus.lat, bus.lng));
+                busMarkers.remove(Integer.toString(bus.bus_id));
+            }
+            updatedBusMarkers.put(Integer.toString(bus.bus_id), bus_marker);
+        }
+        // remove all values from busMarkers Map (NOT a Google Map)
+        for(Marker marker: busMarkers.values()){
+            marker.remove();
+        }
+        // and set to the updatedBusMarkers Map (NOT a Google Map)
+        busMarkers = updatedBusMarkers;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,13 +103,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getExtendedMapAsync(this);
+        mapFragment.getMapAsync(this);
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        busScheduleFragment = new BusScheduleFragment();
+        fragmentTransaction.add(R.id.fragment_container, busScheduleFragment);
+        fragmentTransaction.hide(busScheduleFragment);
+        fragmentTransaction.commit();
+
 
         // loads UCSC_WestSide_Bus_Stops.json file to an array of BusStop Objects which are used
         // to create the busstop markers
         loadJsonFromAsset();
 
-        startBackgroundData();
+       locationHandler.postDelayed(updateMarkers, MARKER_UPDATE_INTERVAL);
     }
 
     @Override
@@ -101,77 +137,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onDestroy();
     }
 
-    // draws map
+   // draws map
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true); // Enables the My Location Layer on the map so users get their current position
-        mMap.setOnMarkerClickListener(this);
 
+        mMap.setOnMarkerClickListener(this);
         // set up coordinates for the center of UCSC and move the camera to there with a zoom level of 15 on startup
         LatLng ucsc = new LatLng(36.991406, -122.060731);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucsc, 15));
         drawBusStopMarkers();
     }
 
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        if (busMarkers.containsValue(marker)) {
-            LatLng position = marker.getPosition();
-            Log.i(TAG, position.toString());
-        }
-        return false;
-    }
-
-    public void stopBackgroundData() {
-        locationHandler.removeCallbacks(updateMarkers);
-    }
-
-    public void startBackgroundData() {
-        locationHandler.postDelayed(updateMarkers, MARKER_UPDATE_INTERVAL);
-    }
-    /* Given a list of xml_markers (defined in TransitInfoXmlParser),
-     * update the map display such that new buses are added as type Marker to mMap,
-     * and previously displayed bus markers are moved to their new coordinates
-     */
-    public void setMarkers(List<Bus> buses) {
-        Map<String, Marker> updatedBusMarkers = new HashMap<String, Marker>();
-
-        for(Bus bus: buses){
-            Log.i(TAG, bus.toString());
-            // if marker exists, move its location
-            Marker bus_marker = busMarkers.get(Integer.toString(bus.bus_id));
-            // if not, add new marker
-            if(bus_marker == null){
-                //ToDo: retrieve stops LatLng from relevant bus stops and store as data in marker for drawing routes
-                //PolylineOptions stops = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
-                bus_marker = mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(bus.lat, bus.lng))
-                        .title(bus.route)
-                        .snippet("Bus ID: " + Integer.toString(bus.bus_id))
-                        .icon(BitmapDescriptorFactory.defaultMarker(bus.color)));
-            }
-            else {
-                bus_marker.animatePosition(new LatLng(bus.lat, bus.lng));
-                busMarkers.remove(Integer.toString(bus.bus_id));
-            }
-            updatedBusMarkers.put(Integer.toString(bus.bus_id), bus_marker);
-        }
-        // remove all values from busMarkers Map (NOT a Google Map)
-        for(Marker marker: busMarkers.values()){
-            marker.remove();
-        }
-        // and set to the updatedBusMarkers Map (NOT a Google Map)
-        busMarkers = updatedBusMarkers;
-    }
-
-    public void toggleStops(View view) {
-        showStops = !showStops;
-        drawBusStopMarkers();
-    }
-
     // draws the busstop markers on the google map
-    private void drawBusStopMarkers() {
+    public void drawBusStopMarkers(){
         Map<String, Marker> updatedVisibleMarkers = new HashMap<String, Marker>();
 
         for (BusStop temp : busStops){
@@ -188,6 +168,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 visibleMarkers.remove(Double.toString(temp.getLatitude() + temp.getLongitude()));
             }
             updatedVisibleMarkers.put(Double.toString(temp.getLatitude()+ temp.getLongitude()), busStop);
+
         }
 
         // remove all values from busMarkers Map (NOT a Google Map)
@@ -198,8 +179,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         visibleMarkers = updatedVisibleMarkers;
     }
 
+
+    public void toggleStops(View view) {
+        if (showStops)
+            showStops = false;
+        else
+            showStops = true;
+        drawBusStopMarkers();
+    }
     // loads bus stops specified by json file
-    private void loadJsonFromAsset() {
+    public void loadJsonFromAsset(){
 
         try{
             InputStream in = getAssets().open("UCSC_Westside_Bus_Stops.json");
@@ -211,5 +200,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             System.out.println("Error reading file");
         }
     }
+
+    Runnable updateMarkers = new Runnable() {
+        @Override
+        public void run(){
+            //networkInfo = cm.getActiveNetworkInfo();
+            //if(networkInfo != null && networkInfo.isConnected()){
+            NetworkActivity networkActivity = new NetworkActivity();
+            networkActivity.load();
+
+            if(busMarkers.isEmpty()) {
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, "connecting...", duration);
+                toast.show();
+            }
+            locationHandler.postDelayed(this, MARKER_UPDATE_INTERVAL);
+        }
+    };
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (busMarkers.containsValue(marker)) {
+            LatLng position = marker.getPosition();
+            Log.i(TAG, position.toString());
+        }
+
+        if(!infoWindowActive) {
+
+            busScheduleFragment.setBusSchedule(marker.getTitle());
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.show(busScheduleFragment);
+            fragmentTransaction.commit();
+            infoWindowActive = true;
+        }
+        else{
+            busScheduleFragment.setBusSchedule(marker.getTitle());
+        }
+        return false;
+    }
+
 
 }
